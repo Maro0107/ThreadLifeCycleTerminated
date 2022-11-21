f587de4c1579c7221446d81881ee03d20b375b94
Marek Fijalkowski <gitmaro1983@gmail.com>
2022-11-21 19:51:47 +0100

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)throws InterruptedException {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Thread run");
+            }
+        });
+        thread.start();
+        Thread.sleep(1);
+        System.out.println("Thread state: " + thread.getState());
+    }
+}
